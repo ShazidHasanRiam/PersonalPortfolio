b73a56a744a739e09d196a947b032391cecb3ea6
shazidhasanriam <shazid.riam@northsouth.edu>
2021-12-09 11:59:56 +0600

    Nav Manipulation

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// show menu y hidden
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// SHOW Menu
+// validate if constant exists
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu Hidden
+// validate if constants exists
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// Remove menu mobile
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
