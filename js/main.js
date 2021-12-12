44697bf5e315880a5c4ce0bcaec5d96b0058cb63
shazidhasanriam <shazid.riam@northsouth.edu>
2021-12-12 20:37:24 +0600

    Done with the skills section

--- js/main.js
+++ js/main.js
@@ -28,3 +28,22 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// SKILLS SCETION
+const skillsContent = document.getElementsByClassName("skills-content");
+const skillsHeader = document.querySelectorAll(".skills-header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills-content skills-close";
+  }
+
+  if (itemClass === "skills-content skills-close") {
+    this.parentNode.className = "skills-content skills-open";
+  }
+}
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
